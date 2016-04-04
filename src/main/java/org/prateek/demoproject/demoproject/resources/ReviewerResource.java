676e75a4cac546901bb1b8c04ba4b6dccca8e62f
Prateek Joshi <prateekjoshi2013@gmail.com>
2016-04-03 21:47:02 -0400

    TopEliteReviewerService and TopEliteReviewer class added

--- src/main/java/org/prateek/demoproject/demoproject/resources/ReviewerResource.java
+++ src/main/java/org/prateek/demoproject/demoproject/resources/ReviewerResource.java
@@ -1,6 +1,7 @@
 package org.prateek.demoproject.demoproject.resources;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -10,8 +11,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.prateek.demoproject.demoproject.model.Business;
+import org.prateek.demoproject.demoproject.model.Review;
 import org.prateek.demoproject.demoproject.model.Reviewer;
+import org.prateek.demoproject.demoproject.model.TopEliteReviewer;
+import org.prateek.demoproject.demoproject.model.TopReviewer;
 import org.prateek.demoproject.demoproject.service.ReviewerService;
+import org.prateek.demoproject.demoproject.service.TopEliteReviewerService;
+import org.prateek.demoproject.demoproject.service.TopReviewerService;
 
 @Path("/reviewer")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -19,13 +25,28 @@ import org.prateek.demoproject.demoproject.service.ReviewerService;
 public class ReviewerResource {
 
 	ReviewerService reviewerService=new ReviewerService();
-
+	TopReviewerService topReviewerService=new TopReviewerService();
+	TopEliteReviewerService topEliteReviewerService=new TopEliteReviewerService();
 	@GET
 	@Path("/{reviewerId}")
 	public Reviewer getReviewer(@PathParam("reviewerId")String reviewerID) throws SQLException{
 		return reviewerService.getReviewer(reviewerID);
 
 
+	}
+
+	//Reviewer/home/{city}/{category}/top_reviewer/{count}
+	@GET
+	@Path("home/{city}/{category}/top_reviewer/{count}")
+	public ArrayList<TopReviewer> getTopReviewer(@PathParam("city")String city,@PathParam("category")String category,@PathParam("count")int count) throws SQLException{
+		return topReviewerService.getTopReviewer(city,category,count);
+
+
+	}
+	@GET
+	@Path("/home/{city}/{category}/elite_reviewer/{count}")
+	public ArrayList<TopEliteReviewer> getTopEliteReviewer(@PathParam("city")String city,@PathParam("category")String category,@PathParam("count")int count) throws SQLException{
+		return topEliteReviewerService.getTopReviewer(city, category, count);
 	}
 
 
