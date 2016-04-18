39a6cb0f92aab86c855fb4e3d033f1d0466769ab
Prateek Joshi <prateekjoshi2013@gmail.com>
2016-04-18 19:31:02 -0400

    trends service and topEliteService updated

--- src/main/java/org/prateek/demoproject/demoproject/service/TopEliteReviewerService.java
+++ src/main/java/org/prateek/demoproject/demoproject/service/TopEliteReviewerService.java
@@ -22,7 +22,7 @@ public class TopEliteReviewerService {
 +" (select a1.user_id,a1.name,a1.friends,b1.city,b1.categories from NTIWARE.REVIEWER a1 "
 +" join (select a.city,a.business_id,b.user_id,a.categories from sharshar.BUSINESS a "
 +" join ARAVI.REVIEWS b on a.business_id=b.business_id"
-+" where city='Las Vegas' AND categories like '%Restaurant%') b1"
++" where city='"+city+"' AND categories like '%"+category+"%') b1"
 +" on a1.USER_ID=b1.USER_ID where a1.elite not like '[]') group by USER_ID,name"
 +" order by count(*) desc) a3 join NTIWARE.REVIEWER b3 on a3.user_id=b3.user_id"
 +" order by a3.usercount desc,b3.REVIEW_COUNT desc) where rownum <"+ (count+1);
